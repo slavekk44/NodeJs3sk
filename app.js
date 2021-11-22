f44b04a57047ea89febe34d2debb84932bada862
slavekk44 <87363097+slavekk44@users.noreply.github.com>
2021-11-22 23:32:13 +0000

    add  app use ruter

--- app.js
+++ app.js
@@ -15,8 +15,17 @@ app.use(express.static("views"));
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   console.log("test ");
-  res.render("index"); //no need for ejs extension });
+  res.render("index"); //no need for ejs extension
 });
+//route for contacts
+app.get("/contacts", (req, res) => {
+  res.render("contacts");
+});
+// add this part ;D
+
+// //pass requests to the router middleware
+// const router = require("./routes/post");
+// app.use(router);
 
 //route for contacts app.get('/contacts', (req, res) => { res.render('contacts'); });
 const port = process.argv[2] || process.env.PORT || 3000;
